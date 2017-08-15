9e8dcee2535c60507cb81013eb2b5f91064e825b
Andrew J Lenards <andrew.lenards@gmail.com>
2017-08-15 12:54:47 -0700

    Remove old elpa packages

--- elpa/helm-mode-manager-1.0.0/helm-mode-manager-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-mode-manager" "1.0.0" "Select and toggle major and minor modes with helm" (quote ((helm "1.5.3"))))
--- elpa/js2-highlight-vars-0.1.0/js2-highlight-vars-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "js2-highlight-vars" "0.1.0" "highlight occurrences of the variable under cursor" (quote ((js2-mode "20150909"))))
